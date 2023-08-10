9738cc486ae7731b71e936401809adcba734528b
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-08-09 23:21:29 -0300

    Empezando nuevo curso de Next

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed dolorum quos dolore sapiente
+        natus, perspiciatis dolores consequatur expedita nam minus laborum temporibus eaque sit nisi
+        nemo delectus architecto doloribus esse, at fugit ipsum commodi. Veritatis eligendi ut odit
+        praesentium facere minus alias! Sit minima, repellat eveniet nobis porro corrupti,
+        consequuntur atque adipisci excepturi fugit nostrum eius nihil dolorum quibusdam dolorem
+        ipsam. Consequuntur quisquam eius corrupti soluta quod nam obcaecati fugiat dolorem
+        praesentium blanditiis, doloremque nulla! Velit, temporibus illo? Odio amet tempore quas?
+        Maiores aliquid quia sequi amet? Corporis veritatis nisi suscipit id, debitis tempore
+        cupiditate a, in optio animi soluta?
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/app/services/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ServicePage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam veritatis porro sed
+        sapiente consectetur dolor saepe voluptatum impedit commodi deleniti consequuntur nulla
+        nesciunt ad asperiores doloribus ipsam dolorum, autem laboriosam harum quaerat doloremque
+        error. Sit aliquid blanditiis unde et mollitia ducimus, molestiae eligendi, molestias
+        corrupti laudantium vitae recusandae optio consequuntur!
+      </p>
+    </div>
+  )
+}
+
+export default ServicePage
